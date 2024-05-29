c4d6ad6bb019af28da89b1bef64e5087b12a21cb
ReneRDZCT <rene.rdzc@outlook.com>
2024-05-29 13:22:02 -0600

    Practica 2 de Git

---
+++ objetos_rarc/src/com/PrincipalDeTelefono.java
@@ -0,0 +1,22 @@
+package com;
+
+public class PrincipalDeTelefono {
+	public static void main(String[] args) {
+		
+		//Creo un objeto de la clase
+		Telefono tl = new Telefono();
+		
+		tl.setColor("Rojo");
+		tl.setCapacidad(256);
+		tl.setMarca("Apple");
+		tl.setModelo(13);
+		tl.setPrecio(28999);
+		
+		System.out.println(tl);
+		
+		
+		
+		
+	}
+
+}

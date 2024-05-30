86fd7a98bcfbd06f64a68d0c8cfeff68fca0e828
VictorRobinWolf <victor.utpmp@gmail.com>
2024-05-30 08:54:36 -0600

    esto es un comentario

---
+++ ejercicio2_vfh/src/com/Principal.java
@@ -0,0 +1,31 @@
+package com;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//tipos primitivos
+
+		  
+		  //numeros enteros
+		  byte b = 127;
+		  short s  = 32456;
+		  int i = 999999999;
+		  long l = 999999999999999999l;
+		  
+		  //decimales
+		  float f = 45.3466f;
+		  double d =3456.5354;
+		  
+		  //caracteres y booleans
+		  char c = ' ';
+		  boolean bol = false;
+		  
+		  //-------------------------------------------
+		  //Wrappers - envoltorios
+		  Byte b1 = 100; // asignacion directa
+		  Short ss = new Short(s); //mediante un constructor
+		  Integer ii = 456465;
+		  Character ch = 'Y';
+		  String s1 = "guoeifvuhewr23523$%&/($%&(%"	;}
+
+}
